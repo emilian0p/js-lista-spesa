a47c97667b1efe737448269104f39562455206a2
emilian0p <102601927+emilian0p@users.noreply.github.com>
2022-06-21 23:15:58 +0200

    finalmente gira

--- main.js
+++ main.js
@@ -1,2 +1,27 @@
+const spesa = [
+    'uova',
+    'pane',
+    'pasta',
+    'latte',
+    'farina',
+    'lievito'
+];
+
+const pezzo= document.getElementById('spesa');
+console.log(spesa);
 
 
+let i=0;
+
+while(i<spesa.length){
+
+    const newElement = document.createElement('li');
+    
+    newElement.innerHTML=spesa[i];
+
+    pezzo.append(newElement);
+    
+    i++;
+
+   
+}
